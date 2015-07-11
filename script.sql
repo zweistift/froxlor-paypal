893f5c4785f9315fce7ff82fb6e90cebd95a1923
zweistift <simon.thomann@gmail.com>
2015-07-11 17:02:25 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,21 @@
+USE FROXLOR;
 
+ALTER TABLE `froxlor`.`panel_customers` 
+ENGINE =InnoDB;
+
+CREATE TABLE `froxlor`.`zw_pay_abo` (
+  `PK_ABO_ID` INT NOT NULL AUTO_INCREMENT,
+  `ABO_KURZ` VARCHAR(10) NULL,
+  `ABO_DESC` VARCHAR(45) NULL,
+  `ABO_COSTS_MTH` DOUBLE NULL,
+  `ABO_COSTS_YEAR` DOUBLE NULL,
+  PRIMARY KEY (`PK_ABO_ID`));
+  
+
+
+
+
+ALTER TABLE `froxlor`.`panel_customers`
+ADD COLUMN `zw_pay_abo_type` INT(10) NULL,
+ADD COLUMN `zw_pay_abo_expire` DATE NULL,
+ADD COLUMN `zw_pay_abo_payed` VARCHAR(45) NULL;
