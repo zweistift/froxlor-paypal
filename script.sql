17ebd44702b83929a48682f92881bb628b02d42a
zweistift <simon.thomann@gmail.com>
2015-07-11 17:02:49 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,8 +1,6 @@
 USE FROXLOR;
-
 ALTER TABLE `froxlor`.`panel_customers` 
 ENGINE =InnoDB;
-
 CREATE TABLE `froxlor`.`zw_pay_abo` (
   `PK_ABO_ID` INT NOT NULL AUTO_INCREMENT,
   `ABO_KURZ` VARCHAR(10) NULL,
@@ -10,11 +8,6 @@ CREATE TABLE `froxlor`.`zw_pay_abo` (
   `ABO_COSTS_MTH` DOUBLE NULL,
   `ABO_COSTS_YEAR` DOUBLE NULL,
   PRIMARY KEY (`PK_ABO_ID`));
-  
-
-
-
-
 ALTER TABLE `froxlor`.`panel_customers`
 ADD COLUMN `zw_pay_abo_type` INT(10) NULL,
 ADD COLUMN `zw_pay_abo_expire` DATE NULL,
