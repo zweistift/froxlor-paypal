b2769eef5568f19ff53c82d1d1e51a2fd6a0db66
zweistift <simon.thomann@gmail.com>
2015-07-12 11:45:12 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -12,3 +12,19 @@ ALTER TABLE `froxlor`.`panel_customers`
 ADD COLUMN `zw_pay_abo_type` INT(10) NULL,
 ADD COLUMN `zw_pay_abo_expire` DATE NULL,
 ADD COLUMN `zw_pay_abo_payed` VARCHAR(45) NULL;
+
+
+
+
+CREATE TABLE `froxlor`.`paypal_notifications` (
+  `info_ID` INT NOT NULL AUTO_INCREMENT,
+  `txn_id` VARCHAR(45) NULL,
+  `payer_id` VARCHAR(13) NULL,
+  `payment_status` VARCHAR(45) NULL,
+  `receiver_mail` VARCHAR(45) NULL,
+  `custom` VARCHAR(45) NULL,
+  `txn_type` VARCHAR(45) NULL,
+  `next_payment_date` VARCHAR(45) NULL,
+  `time_created` VARCHAR(45) NULL,
+  `reccuring_pyament_id` VARCHAR(45) NULL,
+  PRIMARY KEY (`info_ID`));
