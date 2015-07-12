4ff11a1ee643c9f9803d840f080e3c60f0648563
zweistift <simon.thomann@gmail.com>
2015-07-12 11:48:21 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -28,3 +28,17 @@ CREATE TABLE `froxlor`.`paypal_notifications` (
   `time_created` VARCHAR(45) NULL,
   `reccuring_pyament_id` VARCHAR(45) NULL,
   PRIMARY KEY (`info_ID`));
+  
+  CREATE TABLE `froxlor`.`paypal_transactions` (
+  `info_ID` INT NOT NULL AUTO_INCREMENT,
+  `txn_id` VARCHAR(45) NULL,
+  `payer_id` VARCHAR(13) NULL,
+  `payment_status` VARCHAR(45) NULL,
+  `receiver_mail` VARCHAR(45) NULL,
+  `custom` VARCHAR(45) NULL,
+  `txn_type` VARCHAR(45) NULL,
+  `next_payment_date` VARCHAR(45) NULL,
+  `time_created` VARCHAR(45) NULL,
+  `reccuring_pyament_id` VARCHAR(45) NULL,
+  `amount` DOUBLE NULL,
+  PRIMARY KEY (`info_ID`));
