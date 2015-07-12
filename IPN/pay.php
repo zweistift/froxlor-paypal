19701e58719181c747574f8eda992426f7c3d440
zweistift <zweistift@users.noreply.github.com>
2015-07-12 17:54:37 +0200

    Update pay.php

--- IPN/pay.php
+++ IPN/pay.php
@@ -1,104 +1,171 @@
 <?php
-function getPayOverview($froxlor_client_details, $session){
-	$froxlor_login_name = $froxlor_client_details['loginname'];
-    
-    if (isset($froxlor_client_details['customerid'])) {
-        $froxlor_user_id = $froxlor_client_details['customerid'];
-        
-	$froxlor_already_payed = 1;
-    
-    
-	if($froxlor_login_name == "admin"){
-	// do nothing or set userid to web1
-	$froxlor_login_name = "web1";
+// CONFIG: Enable debug mode. This means we'll log requests into 'ipn.log' in the same directory.
+// Especially useful if you encounter network errors or other intermittent problems with IPN (validation).
+// Set this to 0 once you go live or don't require logging.
+define("DEBUG", 1);
+// Set to 0 once you're ready to go live
+define("USE_SANDBOX", 0);
+define("LOG_FILE", "./ipn.log");
+// Read POST data
+// reading posted data directly from $_POST causes serialization
+// issues with array data in POST. Reading raw POST data from input stream instead.
+$raw_post_data = file_get_contents('php://input');
+$raw_post_array = explode('&', $raw_post_data);
+$myPost = array();
+foreach ($raw_post_array as $keyval) {
+	$keyval = explode ('=', $keyval);
+	if (count($keyval) == 2)
+		$myPost[$keyval[0]] = urldecode($keyval[1]);
+}
+// read the post from PayPal system and add 'cmd'
+$req = 'cmd=_notify-validate';
+if(function_exists('get_magic_quotes_gpc')) {
+	$get_magic_quotes_exists = true;
+}
+foreach ($myPost as $key => $value) {
+	if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+		$value = urlencode(stripslashes($value));
+	} else {
+		$value = urlencode($value);
 	}
-
-	$db = new mysqli('localhost', 'root', '20simonX15','froxlor');
-	if($db->connect_errno > 0){
-		return("Verbindung zur Datenbank nicht moeglich. Fehler".$db->connect_error);
+	$req .= "&$key=$value";
+}
+// Post IPN data back to PayPal to validate the IPN data is genuine
+// Without this step anyone can fake IPN data
+if(USE_SANDBOX == true) {
+	$paypal_url = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+} else {
+	$paypal_url = "https://www.paypal.com/cgi-bin/webscr";
+}
+$ch = curl_init($paypal_url);
+if ($ch == FALSE) {
+	return FALSE;
+}
+curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+if(DEBUG == true) {
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
+}
+// CONFIG: Optional proxy configuration
+//curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+// Set TCP timeout to 30 seconds
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
+// CONFIG: Please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set the directory path
+// of the certificate as shown below. Ensure the file is readable by the webserver.
+// This is mandatory for some environments.
+//$cert = __DIR__ . "./cacert.pem";
+//curl_setopt($ch, CURLOPT_CAINFO, $cert);
+$res = curl_exec($ch);
+if (curl_errno($ch) != 0) // cURL error
+	{
+	if(DEBUG == true) {	
+		error_log(date('[Y-m-d H:i e] '). "Can't connect to PayPal to validate IPN message: " . curl_error($ch) . PHP_EOL, 3, LOG_FILE);
 	}
-    $sql = "SELECT * FROM vKundenabos WHERE Kundennummer = ".$froxlor_user_id.";";
-		
-	if(!$result = $db->query($sql)){
-		return("Problem beim Query aufgetreten".$db->error);	
-	}
+	curl_close($ch);
+	exit;
+} else {
+		// Log the entire HTTP response if debug is switched on.
+		if(DEBUG == true) {
+			error_log(date('[Y-m-d H:i e] '). "HTTP request of validation request:". curl_getinfo($ch, CURLINFO_HEADER_OUT) ." for IPN payload: $req" . PHP_EOL, 3, LOG_FILE);
+			error_log(date('[Y-m-d H:i e] '). "HTTP response of validation request: $res" . PHP_EOL, 3, LOG_FILE);
+		}
+		curl_close($ch);
+}
+// Inspect IPN validation result and act accordingly
+// Split response headers and payload, a better way for strcmp
+$tokens = explode("\r\n\r\n", trim($res));
+$res = trim(end($tokens));
+if (strcmp ($res, "VERIFIED") == 0) {
+	// check whether the payment_status is Completed
+	// check that txn_id has not been previously processed
+	// check that receiver_email is your PayPal email
+	// check that payment_amount/payment_currency are correct
+	// process payment and mark item as paid.
+	// assign posted variables to local variables
+    $item_name = "";
+    $item_number = "";
+    $payment_amount = "";
+    $paypal_currency = "";
+    $client = "";
+    $txn_type = "";
+    $payer_id= "";
+    $next_payment_date = "";
+    $recurring_payment_id = "";
+    $subscr_id = "";
+    if (!empty($_POST["item_name"])) {$item_name = $_POST['item_name'];}
+    if (!empty($_POST["item_number"])) {$item_number = $_POST['item_number'];}
+    if (!empty($_POST["mc_gross"])) {$payment_amount = $_POST['mc_gross'];}
+    if (!empty($_POST["mc_currency"])) {$payment_currency = $_POST['mc_currency'];}
+    if (!empty($_POST["custom"])) {$client = $_POST['custom'];}
+    if (!empty($_POST["txn_type"])) {$txn_type = $_POST['txn_type'];}    
+	if (!empty($_POST["payer_id"])) {$payer_id = $_POST['payer_id'];} 
+    if (!empty($_POST["next_payment_date"])) {$next_payment_date = $_POST['next_payment_date'];} 
+    if (!empty($_POST["time_created"])) {$time_created = $_POST['time_created'];} 
+    if (!empty($_POST["recurring_payment_id"])) {$recurring_payment_id = $_POST['recurring_payment_id'];} 
+    if (!empty($_POST["subscr_id"])) {$subscr_id = $_POST['subscr_id'];} 
+	$payment_status = $_POST['payment_status'];
+    $txn_id = $_POST['txn_id'];
+    
+	$receiver_email = $_POST['receiver_email'];
+	$payer_email = $_POST['payer_email'];
 	
-	while($row = $result->fetch_assoc()){
-		$datum = date_create($row['Ablauf']);
-		$payment_expire_date = date_format($datum,"d M Y");
-        $payment_abo_desc = $row['Abo Beschreibung'];
-        $payment_abo_kurz = $row['AboZeichen'];
-        $payment_abo_amount = $row['Kosten'];
-        $payment_abo_amount_year = $payment_abo_amount * 12;
-        $froxlor_already_payed = $row['Bezahlt'];
-	}
-	$button_hdl_small = "<form id=\"paypal\" action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\"><input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\"><input type=\"hidden\" name=\"custom\" value=\"".$froxlor_user_id."\"><input type=\"hidden\" name=\"hosted_button_id\" value=\"RJBFU6EUQTFA4\"><input type=\"image\" src=\"https://www.paypalobjects.com/webstatic/en_AU/i/buttons/btn_paywith_primary_m.png\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\"><img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\"></form>";
-        
-        
-        
-        
-	$button_hdl_medium = "<form id=\"paypal\" action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\"><input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\"><input type=\"hidden\" name=\"custom\" value=\"".$froxlor_user_id."\"><input type=\"hidden\" name=\"hosted_button_id\" value=\"7N9WNQ2A6CZLG\"><input type=\"image\" src=\"https://www.paypalobjects.com/webstatic/en_AU/i/buttons/btn_paywith_primary_m.png\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\"><img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\"></form>";
-        
-    $button_hdl_large = "<form id=\"paypal\" action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\"><input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\"><input type=\"hidden\" name=\"custom\" value=\"".$froxlor_user_id."\"><input type=\"hidden\" name=\"hosted_button_id\" value=\"7N9WNQ2A6CZLG\"><input type=\"image\" src=\"https://www.paypalobjects.com/webstatic/en_AU/i/buttons/btn_paywith_primary_m.png\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\"><img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\"></form>";
-        
-    $button_hdl_xtralarge = "<form id=\"paypal\" action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\"><input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\"><input type=\"hidden\" name=\"custom\" value=\"".$froxlor_user_id."\"><input type=\"hidden\" name=\"hosted_button_id\" value=\"7N9WNQ2A6CZLG\"><input type=\"image\" src=\"https://www.paypalobjects.com/webstatic/en_AU/i/buttons/btn_paywith_primary_m.png\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\"><img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\"></form>";
-        
-    $button_unsubscribe = "<A HREF=\"https://www.paypal.com/cgi-bin/webscr?cmd=_subscr-find&alias=K87NQABLHP6GY\">Abo beenden</A>";
 
-    $button_test = "<form id=\"paypal\" action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\"><input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\"><input type=\"hidden\" name=\"custom\" value=\"".$froxlor_user_id."\"><input type=\"hidden\" name=\"hosted_button_id\" value=\"JBEE6F32NSU6Y\"><input type=\"image\" src=\"https://www.paypalobjects.com/webstatic/en_AU/i/buttons/btn_paywith_primary_m.png\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\"><img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\"></form>";
+    
+    function database($db, $query){
+        if (isset($db) && isset($query)){
+            $connection = new mysqli('localhost', 'root', '20simonX15', $db);
+            if($connection->connect_errno > 0){
+                return("DB connect Error");
+            }
+            $sql = $query;
+            if(!$result = $connection->query($sql)){
+                return("Query Errno");
+            }
+        }
         
-    switch($payment_abo_kurz){
-    case "S":
-        $button = $button_hdl_small;
-        break;
-    case "M":
-        $button = $button_hdl_medium;
-        break;
-    case "L":
-        $button = $button_hdl_large;
-        break;
-    case "XL":
-        $button = $button_hdl_xtralarge;
-        break;
-    case "XXX":
-        $button = $button_test;
-        break;
-    default:
-        $button = $button_hdl_small;
-        break;
+        else{
+            return("You fucking Idiot didn't gave a DB with.");
+        }
     }
     
+    if($payment_status == 'Completed'){
+        $insert = "INSERT INTO paypal_transactions (txn_id, payer_id, payment_status, receiver_mail, custom, txn_type, next_payment_date, time_created, reccuring_payment_id, amount, subscr_id) VALUES (\"".$txn_id."\", \"".$payer_id."\", \"".$payment_status."\", \"".$receiver_email."\", \"".$client."\", \"".$txn_type."\", \"".$next_payment_date."\", \"".$time_created."\", \"".$reccuring_payment_id."\", \"".$payment_amount."\", \"".$subscr_id."\");";
+        database("froxlor", $insert);
 
-    //$button_hdl_small = "<a href="">Mit Paypal bezahlen</a>";
-	$ausgabe = "<table class=\"dboarditem\"><thead><tr><th colspan=\"2\">Abrechnung</th></tr></thead>";
-    $ausgabe = $ausgabe."<tbody>";
-	$ausgabe = $ausgabe."<tr><td>Kundennummer</td><td>00".$froxlor_user_id."</td></tr>";
-    $ausgabe = $ausgabe."<tr><td>Abo-Typ</td><td>".$payment_abo_desc."</td></tr>";
-    $ausgabe = $ausgabe."<tr><td>Session</td><td>".$session."</td></tr>";
-    $ausgabe = $ausgabe."<tr><td>Abokosten</td><td>".$payment_abo_amount." CHF/Monat | ".$payment_abo_amount_year." CHF/Jahr</td></tr>";
-    $ausgabe = $ausgabe."<tr><td>Abo l&auml;uft aus</td><td>".$payment_expire_date."</td></tr>";
-    //$ausgabe = $ausgabe."<tr><td>Zahlungsart</td><td>PayPal <a href=\"#\">Verl&auml;ngern</a></td></tr>";
-    $ausgabe = $ausgabe."<tr><td>Warnung</td><td>Bitte beachten sie dass die Daten zuerst mit unserem System abgeglichen werden!</td></tr>";
-    if($froxlor_already_payed == 1){
-        $ausgabe = $ausgabe."<tr><td>Status</td><td>Abo Aktiv</td></tr>";
-        $ausgabe = $ausgabe."<tr><td>Beenden</td><td>".$button_unsubscribe."</td></tr>";
+        
+
+        
     }
     else{
-        $ausgabe = $ausgabe."<tr><td>Status</td><td>Abo nicht aktiv</td></tr>";
-        $ausgabe = $ausgabe."<tr><td>Aktivation</td><td>".$button."</td></tr>";
-        
+        $insert = "INSERT INTO paypal_notifications (txn_id, payer_id, payment_status, receiver_mail, custom, txn_type, next_payment_date, time_created, reccuring_payment_id, amount) VALUES (\"".$txn_id."\", \"".$payer_id."\", \"".$payment_status."\", \"".$receiver_email."\", \"".$client."\", \"".$txn_type."\", \"".$next_payment_date."\", \"".$time_created."\", \"".$reccuring_payment_id."\", \"".$payment_amount."\", \"".$subscr_id."\");";
+        database("froxlor", $insert);
     }
-    $ausgabe = $ausgabe."<tbody></table>";
-	return $ausgabe;
-    }
-    else{
-        $froxlor_user_id = 0;
-        return "";
-    }
-	
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+	if(DEBUG == true) {
+		error_log(date('[Y-m-d H:i e] '). "Verified IPN: $req ". PHP_EOL, 3, LOG_FILE);
+	}
+} else if (strcmp ($res, "INVALID") == 0) {
+	// log for manual investigation
+	// Add business logic here which deals with invalid IPN messages
+	if(DEBUG == true) {
+		error_log(date('[Y-m-d H:i e] '). "Invalid IPN: $req" . PHP_EOL, 3, LOG_FILE);
+	}
 }
 ?>
-
-
-
