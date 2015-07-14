38e0134570fa6456bcaaac4e127da7a8dccb5bc2
Simon <zweistift@users.noreply.github.com>
2015-07-14 13:20:37 +0200

    Update admin_billing_overview.php

--- admin_billing_overview.php
+++ admin_billing_overview.php
@@ -1,19 +1,5 @@
 <?php
-/**
- * filename: $Source: /syscp/modules_admin_sasettings.php,v $
- * begin: Thursday, Dec 21 2004
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version. This program is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * @author Wolfgang Ziegler <nuppla@gmx.at>
- * @copyright 2004 sasettings dev team
- */
+
 
     define('AREA', 'admin');
 
